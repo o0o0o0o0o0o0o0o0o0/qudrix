62f184c6a57ec607849dd1dc8cb9873f79d95bc5
AndriiBulhakov <andrijbulgakov5@gmail.com>
2023-10-27 16:39:14 +0200

    logic for params

--- src/main.js
+++ src/main.js
@@ -1,49 +1,87 @@
 import './styles/style.css'
-import Lenis from '@studio-freight/lenis'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger' // Updated import
 
-// Register ScrollTrigger plugin
-gsap.registerPlugin(ScrollTrigger);
+const radioLists = document.querySelectorAll('.wizard-sidebar__elements-list');
+const codeElement = document.querySelector('pre');
+const wizardTabLinks = document.querySelectorAll('.wizard-tab__nav-link div');
+const wizardTab  = document.querySelectorAll('.wizard-tab__content-item');
 
-// smooth scroll
-const lenis = new Lenis()
+const wizardParametrs = {};
 
-lenis.on('scroll', ScrollTrigger.update)
-
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000)
-})
-
-gsap.ticker.lagSmoothing(0)
-
-// horizontal scroll
-const horizontalItems = document.querySelectorAll('.horizontal-item')
-const height = document.querySelector('.horizontal-sticky__content').offsetWidth
-const width = (horizontalItems.length - 1) * 100
-
-document.querySelector('.horizontal-section').style.height = `${height/16}rem`
-
-gsap.to('.horizontal-item', {
-  xPercent: -width,
-  ease: 'none',
-  scrollTrigger: {
-    trigger: '.horizontal-section',
-    start: 'top top',
-    end: 'bottom bottom',
-    scrub: true,
-  },
-})
-
-horizontalItems.forEach(item => {
-  gsap.to(item.querySelectorAll('.image-full'), {
-    xPercent: 25,
-    ease: 'none',
-    scrollTrigger: {
-      trigger: '.horizontal-section',
-      start: 'top top',
-      end: 'bottom bottom',
-      scrub: true,
+radioLists.forEach(radioList => {
+  // on click on list check if event target is label or radio button
+  radioList.addEventListener('click', (e) => {
+    if (e.target.tagName === 'LABEL') {
+      radioList.querySelectorAll('label').forEach(label => {
+        label.classList.remove('active');
+      });
+      e.target.classList.add('active');
     }
-  })
-})
+    if (e.target.tagName === 'INPUT') {
+      radioList.querySelectorAll('label').forEach(label => {
+        label.classList.remove('active');
+      });
+      e.target.parentElement.classList.add('active');
+    }
+  });
+});
+
+// based on wizardTabLinks text content add this text to wizardParametrs object
+wizardTabLinks.forEach((link, i) => {
+  // if it's not Summury
+  if (link.textContent !== 'Summary') {
+    wizardParametrs[link.textContent.toLocaleLowerCase()] = {};
+    wizardTab[i].setAttribute('data-tab', link.textContent.toLocaleLowerCase());
+  }
+});
+
+// fuctionn to update codeElement content based on wizardParametrs object
+const updateCodeElement = () => {
+  codeElement.textContent = JSON.stringify(wizardParametrs, null, 2).replace(/,/g, ',\n');
+}
+
+
+// get elements with data-tab attribute
+const wizardTabContent = document.querySelectorAll('[data-tab]');
+
+// check inside wizardTabContent if it has elements with attr data-side and add side to wizardParametrs with the same key as data-tab
+wizardTabContent.forEach(tab => {
+  const wizardTabSide = tab.querySelectorAll('[data-side]');
+  wizardTabSide.forEach(side => {
+    wizardParametrs[tab.dataset.tab][side.dataset.side] = {};
+    // add inside each side param keys "basic options" and "variations" with {} as value
+    wizardParametrs[tab.dataset.tab][side.dataset.side]['basic-options'] = {};
+    wizardParametrs[tab.dataset.tab][side.dataset.side]['variations'] = {};
+    // add event listener to each side on click
+    side.addEventListener('click', (e) => {
+      if (e.target.tagName === 'INPUT') {
+        const dataOption = e.target.parentElement.getAttribute('data-option');
+        const dataName = e.target.parentElement.getAttribute('data-name');
+
+        if (dataOption === 'basic-options') {
+          if (wizardParametrs[tab.dataset.tab][side.dataset.side]['basic-options'].hasOwnProperty(dataName)) {
+            // Element exists, replace its value with a new name
+            wizardParametrs[tab.dataset.tab][side.dataset.side]['basic-options']["element-name"] = dataName;
+          } else {
+            // Element doesn't exist, add a new element
+            wizardParametrs[tab.dataset.tab][side.dataset.side]['basic-options']["element-name"] = dataName;
+          }
+        } else {
+          if (wizardParametrs[tab.dataset.tab][side.dataset.side]['variations'].hasOwnProperty(dataName)) {
+            // Element exists, replace its value with a new name
+            wizardParametrs[tab.dataset.tab][side.dataset.side]['variations']["element-name"] = dataName;
+          } else {
+            // Element doesn't exist, add a new element
+            wizardParametrs[tab.dataset.tab][side.dataset.side]['variations']["element-name"] = dataName;
+          }
+        }
+        updateCodeElement();
+      }
+    });
+  });
+});
+
+
+
+
+
+updateCodeElement();
